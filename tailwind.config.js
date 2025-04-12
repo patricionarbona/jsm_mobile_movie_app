58ffae43fbd3ed516f5faad41c441d1a687902d3
patricionarbona <patricioinarbona@gmail.com>
2025-04-12 18:46:29 +0200

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: "#D6C6FF",
+          200: '#A8B5DB',
+          300: '#9ca4ab',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+        },
+        accent: '#ab8bff',
+      }
+    },
   },
   plugins: [],
 }
